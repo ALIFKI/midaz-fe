892dfdaa2a608e51b5cb348595fde9733c0014a8
ALIFKI <alif@roots.co.id>
2023-10-19 01:16:33 +0700

    update svg wa button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { ReactElement, useEffect, useRef, useState } from 'react'
 import Lenis from '@studio-freight/lenis'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import NavbarComponents from '@/components/navbar'
+import WhatsappIcon from '../assets/icon/Web/ic_wa.svg'
+import Image from 'next/image'
 
 
 gsap.registerPlugin(ScrollTrigger)
@@ -13,7 +15,7 @@ export default function Home() {
   const circle = useRef(null);
   const [scroll,setScrol] = useState(0);
   const [isVideoPlay,setVideoPlay] = useState(0);
-  const [animatedClass,setAnimatedClass] = useState('opacity-0 bottom-[18%]')
+  const [animatedClass,setAnimatedClass] = useState('opacity-0 bottom-[17%]')
   // const configLenis = ()=>{
   //   const lenis = new Lenis()
 
@@ -68,8 +70,8 @@ export default function Home() {
       }else{
         console.log(isVideoPlay,'yes')
         setTimeout(() => {
-          setAnimatedClass('opacity-1 bottom-[22%]')
-        }, 37000);
+          setAnimatedClass('opacity-1 bottom-[20%]')
+        }, 35000);
         setVideoPlay(1);
         playVideo();
       }
@@ -98,13 +100,16 @@ export default function Home() {
     <div className='bg-[#0E1B37] relative min-h-screen overflow-scroll w-full'>
         <NavbarComponents></NavbarComponents>
         <div className="flex flex-column z-1 relative w-full section-2 justify-center items-center">
-          <div className="relative w-full justify-center items-center flex flex-row bg-[#0E1B37] overflow-hidden md:h-[100vh]">
+          <div className="relative w-full justify-center items-center flex flex-row bg-[#0E1B37] overflow-hidden md:h-[95vh]">
             <video muted={true} id="v0" tabIndex={0} preload="preload" className='w-[100%]'>
               <source type="video/mp4" src={"https://res.cloudinary.com/dq0yefdvv/video/upload/v1697633480/x0equ2ndoixujm5rqnhg.mp4"}></source>
             </video>
 
             <span className={`absolute translate-x-[-50%] left-[50%] transition-all ${animatedClass} ${isVideoPlay}`}>For reservations, please reach us via <a className='text-golden' href="" style={{transitionDuration : '1s'}}>Contact Page</a></span>
           </div>
+        </div>
+        <div className="fixed flex bg-[#1B2C45] h-[80px] w-[80px] justify-center items-center rounded-[40px] max-lg:bottom-[80px] md:bottom-4 right-[10px]">
+          <Image src={'/../../ic_wa.png'} width={40} height={40} alt={''}></Image>
         </div>
     </div>
   )
